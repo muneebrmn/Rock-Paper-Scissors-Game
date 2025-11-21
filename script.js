f0aa5851af01974b880af2b482b08c23aef6ed81
muneebrmn <muneebrmn19@gmail.com>
2025-11-22 04:40:27 +0500

    fix: update image source URLs to use relative paths

--- script.js
+++ script.js
@@ -15,24 +15,24 @@ rock.addEventListener('click', function() {
     choices.style.display = 'none';
     result.style.display = 'flex';
     user.style.borderColor = "#e52447";
-    document.querySelector('#me img').src = 'https://muneebrmn.github.io/assets/images/rock.svg';
+    document.querySelector('#me img').src = './assets/images/rock.svg';
 
     computerChoice = Math.floor(Math.random() * 3);
 
     if (computerChoice === 0) {
         pc.style.borderColor = "#e52447";
-        document.querySelector('#pc img').src = 'https://muneebrmn.github.io/assets/images/rock.svg';
+        document.querySelector('#pc img').src = './assets/images/rock.svg';
         text.innerHTML = "IT'S A TIE";
     }
     else if (computerChoice === 1) {
         pc.style.borderColor = "#4B68F5";
-        document.querySelector('#pc img').src = 'https://muneebrmn.github.io/assets/images/paper.svg';
+        document.querySelector('#pc img').src = './assets/images/paper.svg';
         text.innerHTML = "YOU LOSE";
         score.innerHTML = parseInt(score.innerHTML) - 1;
     }
     else {
         pc.style.borderColor = "#ECA215";
-        document.querySelector('#pc img').src = 'https://muneebrmn.github.io/assets/images/scissors.svg';
+        document.querySelector('#pc img').src = './assets/images/scissors.svg';
         text.innerHTML = "YOU WIN";
         score.innerHTML = parseInt(score.innerHTML) + 1;
     }
@@ -42,24 +42,24 @@ paper.addEventListener('click', function() {
     choices.style.display = 'none';
     result.style.display = 'flex';
     user.style.borderColor = "#4B68F5";
-    document.querySelector('#me img').src = 'https://muneebrmn.github.io/assets/images/paper.svg';
+    document.querySelector('#me img').src = './assets/images/paper.svg';
 
     computerChoice = Math.floor(Math.random() * 3);
 
     if (computerChoice === 0) {
         pc.style.borderColor = "#e52447";
-        document.querySelector('#pc img').src = 'https://muneebrmn.github.io/assets/images/rock.svg';
+        document.querySelector('#pc img').src = './assets/images/rock.svg';
         text.innerHTML = "YOU WIN";
         score.innerHTML = parseInt(score.innerHTML) + 1;
     }
     else if (computerChoice === 1) {
         pc.style.borderColor = "#4B68F5";
-        document.querySelector('#pc img').src = 'https://muneebrmn.github.io/assets/images/paper.svg';
+        document.querySelector('#pc img').src = './assets/images/paper.svg';
         text.innerHTML = "IT'S A TIE";
     }
     else {
         pc.style.borderColor = "#ECA215";
-        document.querySelector('#pc img').src = 'https://muneebrmn.github.io/assets/images/scissors.svg';
+        document.querySelector('#pc img').src = './assets/images/scissors.svg';
         text.innerHTML = "YOU LOSE";
         score.innerHTML = parseInt(score.innerHTML) - 1;
     }
@@ -69,25 +69,25 @@ scissors.addEventListener('click', function() {
     choices.style.display = 'none';
     result.style.display = 'flex';
     user.style.borderColor = "#ECA215";
-    document.querySelector('#me img').src = 'https://muneebrmn.github.io/assets/images/scissors.svg';
+    document.querySelector('#me img').src = './assets/images/scissors.svg';
 
     computerChoice = Math.floor(Math.random() * 3);
 
     if (computerChoice === 0) {
         pc.style.borderColor = "#e52447";
-        document.querySelector('#pc img').src = 'https://muneebrmn.github.io/assets/images/rock.svg';
+        document.querySelector('#pc img').src = './assets/images/rock.svg';
         text.innerHTML = "YOU LOSE";
         score.innerHTML = parseInt(score.innerHTML) - 1;
     }
     else if (computerChoice === 1) {
         pc.style.borderColor = "#4B68F5";
-        document.querySelector('#pc img').src = 'https://muneebrmn.github.io/assets/images/paper.svg';
+        document.querySelector('#pc img').src = './assets/images/paper.svg';
         text.innerHTML = "YOU WIN";
         score.innerHTML = parseInt(score.innerHTML) + 1;
     }
     else {
         pc.style.borderColor = "#ECA215";
-        document.querySelector('#pc img').src = 'https://muneebrmn.github.io/assets/images/scissors.svg';
+        document.querySelector('#pc img').src = './assets/images/scissors.svg';
         text.innerHTML = "IT'S A TIE";
     }
 });
